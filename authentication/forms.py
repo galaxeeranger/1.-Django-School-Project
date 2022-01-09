3c96138a1697e7f1b18e1a2ae05a8e421517a9de
galaxeeranger <96517689+galaxeeranger@users.noreply.github.com>
2022-01-09 18:38:29 +0530

    Add files via upload

---
+++ authentication/forms.py
@@ -0,0 +1,142 @@
+from django import forms
+from authentication.models import Images
+from authentication.models import Images, SessionYearModel
+
+
+class DateInput(forms.DateInput):
+    input_type = "date"
+
+
+class AddStudentForm(forms.Form):
+    email = forms.EmailField(label="Email", max_length=50, widget=forms.EmailInput(attrs={"class":"form-control"}))
+    password = forms.CharField(label="Password", max_length=50, widget=forms.PasswordInput(attrs={"class":"form-control"}))
+    first_name = forms.CharField(label="First Name", max_length=50, widget=forms.TextInput(attrs={"class":"form-control"}))
+    last_name = forms.CharField(label="Last Name", max_length=50, widget=forms.TextInput(attrs={"class":"form-control"}))
+    username = forms.CharField(label="Username", max_length=50, widget=forms.TextInput(attrs={"class":"form-control"}))
+    address = forms.CharField(label="Address", max_length=50, widget=forms.TextInput(attrs={"class":"form-control"}))
+
+    #For Displaying Images
+    try:
+        image = Images.objects.all()
+        picture_list = []
+        for picture in image:
+            single_picture = (Images.id, image.image_name)
+            picture_list.append(single_picture)
+    except:
+        picture_list = []
+
+    
+    #For Displaying Session Years
+    try:
+        session_years = SessionYearModel.objects.all()
+        session_year_list = []
+        for session_year in session_years:
+            single_session_year = (session_year.id, str(session_year.session_start_year)+" to "+str(session_year.session_end_year))
+            session_year_list.append(single_session_year)
+            
+    except:
+        session_year_list = []
+    
+    gender_list = (
+        ('Male','Male'),
+        ('Female','Female')
+    )
+    
+    Image_id = forms.ChoiceField(label="Image", choices=picture_list, widget=forms.Select(attrs={"class":"form-control"}))
+    gender = forms.ChoiceField(label="Gender", choices=gender_list, widget=forms.Select(attrs={"class":"form-control"}))
+    session_year_id = forms.ChoiceField(label="Session Year", choices=session_year_list, widget=forms.Select(attrs={"class":"form-control"}))
+    # session_start_year = forms.DateField(label="Session Start", widget=DateInput(attrs={"class":"form-control"}))
+    # session_end_year = forms.DateField(label="Session End", widget=DateInput(attrs={"class":"form-control"}))
+    profile_pic = forms.FileField(label="Profile Pic", required=False, widget=forms.FileInput(attrs={"class":"form-control"}))
+
+
+
+class EditStudentForm(forms.Form):
+    email = forms.EmailField(label="Email", max_length=50, widget=forms.EmailInput(attrs={"class":"form-control"}))
+    password = forms.CharField(label="Password", max_length=50, widget=forms.PasswordInput(attrs={"class":"form-control"}))
+    first_name = forms.CharField(label="First Name", max_length=50, widget=forms.TextInput(attrs={"class":"form-control"}))
+    last_name = forms.CharField(label="Last Name", max_length=50, widget=forms.TextInput(attrs={"class":"form-control"}))
+    username = forms.CharField(label="Username", max_length=50, widget=forms.TextInput(attrs={"class":"form-control"}))
+    address = forms.CharField(label="Address", max_length=50, widget=forms.TextInput(attrs={"class":"form-control"}))
+
+    #For Displaying Images
+    try:
+        image = Images.objects.all()
+        picture_list = []
+        for picture in image:
+            single_picture = (Images.id, image.image_name)
+            picture_list.append(single_picture)
+    except:
+        picture_list = []
+
+    
+    #For Displaying Session Years
+    try:
+        session_years = SessionYearModel.objects.all()
+        session_year_list = []
+        for session_year in session_years:
+            single_session_year = (session_year.id, str(session_year.session_start_year)+" to "+str(session_year.session_end_year))
+            session_year_list.append(single_session_year)
+            
+    except:
+        session_year_list = []
+    
+    gender_list = (
+        ('Male','Male'),
+        ('Female','Female')
+    )
+    
+    Image_id = forms.ChoiceField(label="Image", choices=picture_list, widget=forms.Select(attrs={"class":"form-control"}))
+    gender = forms.ChoiceField(label="Gender", choices=gender_list, widget=forms.Select(attrs={"class":"form-control"}))
+    session_year_id = forms.ChoiceField(label="Session Year", choices=session_year_list, widget=forms.Select(attrs={"class":"form-control"}))
+    # session_start_year = forms.DateField(label="Session Start", widget=DateInput(attrs={"class":"form-control"}))
+    # session_end_year = forms.DateField(label="Session End", widget=DateInput(attrs={"class":"form-control"}))
+    profile_pic = forms.FileField(label="Profile Pic", required=False, widget=forms.FileInput(attrs={"class":"form-control"}))
+    
+
+# from django import forms
+# from django.contrib.auth.forms import UserCreationForm
+
+# from authentication.models import User
+# from django.db import transaction
+# from .models import Student,Teacher,User
+
+
+
+# class StudentSignupForm(UserCreationForm):
+#     first_name = forms.CharField(required=True)
+#     last_name = forms.CharField(required=True)
+#     phone_number = forms.CharField(required=True)
+
+#     class Meta(UserCreationForm.Meta):
+#         model = User
+
+#     @transaction.atomic
+#     def data_save(self):
+#         User = super().save(commit=False)
+#         User.first_name = self.cleaned_data.get('first_name')
+#         User.last_name = self.cleaned_data.get('last_name')
+#         User.save()
+#         student = Student.objects.create(User=User)
+#         student.phone_number = self.cleaned_data.get('phone_number')
+#         student.save()
+#         return student
+
+# class TeacherSignupForm(UserCreationForm):
+#     first_name = forms.CharField(required=True)
+#     last_name = forms.CharField(required=True)
+#     phone_number = forms.CharField(required=True)
+
+#     class Meta(UserCreationForm.Meta):
+#         model = User
+
+#     @transaction.atomic
+#     def data_save(self):
+#         User = super().save(commit=False)
+#         User.first_name = self.cleaned_data.get('first_name')
+#         User.last_name = self.cleaned_data.get('last_name')
+#         User.save()
+#         teacher = Teacher.objects.create(User=User)
+#         teacher.phone_number = self.cleaned_data.get('phone_number')
+#         teacher.save()
+#         return teacher
