2de4037ca33659ce47b1428fb3a989c5cbe2f518
galaxeeranger <96517689+galaxeeranger@users.noreply.github.com>
2022-01-10 09:24:54 +0530

    Add files via upload

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,6 +1,5 @@
-from django.contrib import admin
-from django.urls import path, include
-from . import views
+from django.urls import path
+from . import views, HodViews, StaffViews, StudentViews
 
 
 urlpatterns = [
@@ -8,4 +7,44 @@ urlpatterns = [
     path('signup', views.signup, name="signup"),
     path('signin', views.signin, name="signin"),
     path('signout', views.signout, name="signout"),
+    path('signup/',views.register, name="register"),
+    #url for hod views
+    path('add_staff/', HodViews.add_staff, name="add_staff"),
+    path('add_staff_save/', HodViews.add_staff_save, name="add_staff_save"),
+    path('manage_staff/', HodViews.manage_staff, name="manage_staff"),
+    path('edit_staff/<staff_id>/', HodViews.edit_staff, name="edit_staff"),
+    path('edit_staff_save/', HodViews.edit_staff_save, name="edit_staff_save"),
+    path('delete_staff/<staff_id>/', HodViews.delete_staff, name="delete_staff"),
+    path('manage_session/', HodViews.manage_session, name="manage_session"),
+    path('add_session/', HodViews.add_session, name="add_session"),
+    path('add_session_save/', HodViews.add_session_save, name="add_session_save"),
+    path('edit_session/<session_id>', HodViews.edit_session, name="edit_session"),
+    path('edit_session_save/', HodViews.edit_session_save, name="edit_session_save"),
+    path('delete_session/<session_id>/', HodViews.delete_session, name="delete_session"),
+    path('add_student/', HodViews.add_student, name="add_student"),
+    path('edit_student/<student_id>', HodViews.edit_student, name="edit_student"),
+    path('edit_student_save/', HodViews.edit_student_save, name="edit_student_save"),
+    path('manage_student/', HodViews.manage_student, name="manage_student"),
+    path('delete_student/<student_id>/', HodViews.delete_student, name="delete_student"),
+    path('add_image/', HodViews.add_image, name="add_image"),
+    path('manage_image/', HodViews.manage_image, name="manage_image"),
+    path('delete_image/<image_id>/', HodViews.delete_image, name="delete_image"),
+    path('check_email_exist/', HodViews.check_email_exist, name="check_email_exist"),
+    path('check_username_exist/', HodViews.check_username_exist, name="check_username_exist"),
+    path('admin_profile/', HodViews.admin_profile, name="admin_profile"),
+    path('admin_profile_update/', HodViews.admin_profile_update, name="admin_profile_update"),
+
+    #url for staff\teachers views
+    path('staff_home/', StaffViews.staff_home, name="staff_home"),
+    path('staff_profile/', StaffViews.staff_profile, name="staff_profile"),
+    path('staff_profile_update/', StaffViews.staff_profile_update, name="staff_profile_update"),
+    path('staff_add_result/', StaffViews.staff_add_result, name="staff_add_result"),
+    path('staff_add_result_save/', StaffViews.staff_add_result_save, name="staff_add_result_save"),
+    
+    #url for student views
+    path('student_home/', StudentViews.student_home, name="student_home"),
+    path('student_profile/', StudentViews.student_profile, name="student_profile"),
+    path('student_profile_update/', StudentViews.student_profile_update, name="student_profile_update"),
+    path('student_view_result/', StudentViews.student_view_result, name="student_view_result"), 
+
 ]
